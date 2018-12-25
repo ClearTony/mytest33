66464165ba3b99724f27e43d9ae2cdb8f0f2a385
cjt <903984996@qq.com>
2018-12-25 21:09:17 +0800

    第一次

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+/*
+  Created by IntelliJ IDEA.
+  User: Tony
+  Date: 2018/12/25
+  Time: 21:06
+  To change this template use File | Settings | File Templates.
+*/
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("sda");
+    }
+}
